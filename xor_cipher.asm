7f8acab81377f4ad528229fcb42dc757dce8d078
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-07-28 06:08:36 -0500

    Create xor_cipher.asm

---
+++ xor_cipher.asm
@@ -0,0 +1,37 @@
+section .text
+global _start
+
+_start:
+    mov ecx, 11      ; number of characters to encrypt
+
+    ; Calculate the XOR key (remainder)
+    mov eax, 23
+    mov ebx, 5
+    xor edx, edx   ; Clear EDX for the division operation
+    div ebx
+
+loop1:
+    push ecx
+
+    mov ebx, msg    ; start of message
+    add ebx, ecx
+    sub ebx, 1
+    mov al, [ebx]   ; address of current character
+    xor al, dl
+    mov [ebx], al
+    pop ecx
+    loop loop1
+
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80        ; print message
+
+    mov eax, 1
+    int 0x80        ; exit
+
+section .data
+
+msg db "Kfool8Tlqog", 13, 10
+len equ $ - msg
